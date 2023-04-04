3662181b3307971104017eddf6732cdca15cd461
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-04-04 21:51:03 +0545

    Responsive Changes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,16 @@
-import React, { useState ,useRef, useEffect} from 'react';
-import { Button, Stack, Typography,Input } from '@mui/material';
+import React, { useState, useRef, useEffect } from 'react';
+import { Button, Stack, Typography, useMediaQuery, Badge } from '@mui/material';
 import { styled, useTheme } from '@mui/material/styles';
 import SearchIcon from '@mui/icons-material/Search';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 
 export default function NavBar() {
 
     const theme = useTheme();
-
     const InputRef = useRef(null);
+    const matches = useMediaQuery('(min-width:1200px)');
 
 
     const AppBar = styled(Stack)(({ theme }) => ({
@@ -19,39 +22,73 @@ export default function NavBar() {
         position: "sticky",
         top: "0px",
         zIndex: 3,
-        flexDirection: "row"
+        flexDirection: "row",
+        [theme.breakpoints.down('smd')]: {
+            padding: "20px 3%",
+            width: "94%",
+        },
+        [theme.breakpoints.down('sm')]: {
+            display: "none"
+        }
+    }))
+
+    const MobileAppBar = styled(Stack)(({ theme }) => ({
+        display: "none",
+        backgroundColor: theme.palette.primary.mid,
+        padding: "20px 5%",
+        width: "90%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        [theme.breakpoints.down('sm')]: {
+            display: "flex"
+        }
     }))
 
     return (
-        <AppBar id='app'>
-            <Typography variant='h1' sx={{ letterSpacing: "-1px", fontSize: "30px", color: "text.main" }}>
-                E-SHOP
-            </Typography>
-            <Stack sx={{ flexDirection: "row", width: "40%", backgroundColor: "primary.light", padding: "5px 20px", borderRadius: "12px", border: `1px solid ${theme.palette.text.light}`, alignItems: "center", gap: "5px" }}>
-                <input placeholder='Search your Products' ref={InputRef} style={{
-                    outline: "none", border: "none", backgroundColor: `transparent`, fontSize: "14px", color: `${theme.palette.text.mid}`, fontWeight: 400, width:"100%"
-                }}/>
-                <SearchIcon sx={{ color: "text.mid",cursor:"pointer" }} onClick={()=>{
-                    console.log(InputRef.current.value);
-                }}/>
-                
-            </Stack>
-            
-            <Stack sx={{ flexDirection: "row", alignItems: "center", gap: "80px" }}>
-                <Typography variant='h4' sx={{ fontSize: "16px", color: "text.mid" }}>Home</Typography>
-                <Button disableRipple sx={{
-                    color: "text.mid", fontSize: "16px", border: `1px solid ${theme.palette.text.mid}`, padding: "5px 40px",
-                    backgroundColor: "transparent", borderRadius: "12px",
-                    transition: ".5s",
-                    ":hover": {
-                        backgroundColor: "text.mid",
-                        color: "primary.mid"
-                    }
+        <>
+            <AppBar id='app'>
+                <Typography variant='h1' sx={{ letterSpacing: "-1px", fontSize: "30px", color: "text.main" }}>
+                    E-SHOP
+                </Typography>
+                <Stack sx={{ flexDirection: "row", width: "40%", backgroundColor: "primary.light", padding: "5px 20px", borderRadius: "12px", border: `1px solid ${theme.palette.text.light}`, alignItems: "center", gap: "5px" }}>
+                    <input placeholder='Search your Products' ref={InputRef} style={{
+                        outline: "none", border: "none", backgroundColor: `transparent`, fontSize: "14px", color: `${theme.palette.text.mid}`, fontWeight: 400, width: "100%"
+                    }} />
+                    <SearchIcon sx={{ color: "text.mid", cursor: "pointer" }} onClick={() => {
+                        console.log(InputRef.current.value);
+                    }} />
 
-                }}>
-                    Log in
-                </Button>
-            </Stack>
-        </AppBar>
+                </Stack>
+                <Stack sx={{ flexDirection: "row", alignItems: "center", gap: `${!matches ? "40px" : "80px"}` }}>
+                    <Typography variant='h4' sx={{ fontSize: "16px", color: "text.mid" }}>Home</Typography>
+                    <Button disableRipple sx={{
+                        color: "text.mid", fontSize: "16px", border: `1px solid ${theme.palette.text.mid}`, padding: "5px 40px",
+                        backgroundColor: "transparent", borderRadius: "12px",
+                        transition: ".5s",
+                        ":hover": {
+                            backgroundColor: "text.mid",
+                            color: "primary.mid"
+                        }
+
+                    }}>
+                        Log in
+                    </Button>
+                </Stack>
+            </AppBar>
+            <MobileAppBar>
+                <MenuRoundedIcon sx={{ color: "text.main", fontSize: "30px" }} />
+                <Stack sx={{ flexDirection: "row", gap: "30px" }}>
+                    <SearchRoundedIcon sx={{ color: "text.mid", fontSize: "30px" }} />
+                    <Badge sx={{
+                        "& .MuiBadge-badge": {
+                            color: "primary.light",
+                            backgroundColor: "otherColor.success"
+                        }
+                    }} badgeContent={2} >
+                        <ShoppingCartRoundedIcon sx={{ color: "text.mid", fontSize: "30px" }} />
+                    </Badge>
+                </Stack>
+            </MobileAppBar>
+        </>
     )
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,26 +4,48 @@ import DealerNavigation from '../Components/DealerNavigation';
 import ProductIcon from '../assets/icons/ProductIcon.svg';
 import { styled, useTheme } from '@mui/material/styles';
 import React from 'react'
-import { Star } from '@mui/icons-material';
+import StarRoundedIcon from '@mui/icons-material/StarRounded';
 
 export default function HomePage() {
 
     const theme = useTheme();
 
+    const SingleProductContainer = styled(Stack)(({ theme }) => ({
+        padding: "10px",
+        width: "180px",
+        backgroundColor: "white",
+        borderRadius: "12px",
+        gap: "5px",
+        boxShadow: "rgba(0, 0, 0, 0.02) 0px 0px 0px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px",
+        ":hover": {
+            filter: "drop-shadow(0px 1px 2px rgba(0, 0, 0, 0.25))",
+            boxShadow: "none"
+        },
+        cursor: "pointer",
+        [theme.breakpoints.down('md')]: {
+            width: "200px",
+            backgroundColor:"red"
+        },
+    }))
+
     const SingleProduct = () => {
         return (
-            <Stack sx={{ padding: "10px", width: "180px",backgroundColor:"white",borderRadius:"12px",gap:"5px"}}>
+            <SingleProductContainer >
                 <img src={"https://manasluguitar.com/wp-content/uploads/2020/09/manaslu-atal-guitar-01.jpg"} style={{ width: "100%", height: "180px", objectFit: "cover" }} />
-                <Typography variant='h4' sx={{color:"text.mid",fontSize:"16px"}}>
+                <Typography variant='h4' sx={{ color: "text.mid", fontSize: "16px" }}>
                     Manaslu Atal Semi Acoustic Guitar
                 </Typography>
-                <Typography variant='h3' sx={{color:"secondary.main",fontSize:"16px"}}>
+                <Typography variant='h3' sx={{ color: "secondary.main", fontSize: "16px" }}>
                     Rs 20,000
-                </Typography> 
-                <Stack>
-                    <Star sx={{backgroundColor:"red"}}/>
+                </Typography>
+                <Stack sx={{ flexDirection: "row" }}>
+                    <StarRoundedIcon sx={{ color: `${theme.palette.otherColor.golden}` }} />
+                    <StarRoundedIcon sx={{ color: `${theme.palette.otherColor.golden}` }} />
+                    <StarRoundedIcon sx={{ color: `${theme.palette.otherColor.golden}` }} />
+                    <StarRoundedIcon sx={{ color: `${theme.palette.otherColor.golden}` }} />
+                    <StarRoundedIcon sx={{ color: `${theme.palette.primary.main}` }} />
                 </Stack>
-            </Stack>
+            </SingleProductContainer>
         )
     }
 
@@ -32,13 +54,21 @@ export default function HomePage() {
             <CategoryNavigation />
             <Stack sx={{ marginLeft: "30%", marginTop: "calc(-100vh + 80px)", width: "70%", backgroundColor: "primary.light" }}>
                 <DealerNavigation />
-                <Stack sx={{ height: "150vh", padding: "0px 5%", gap: "15px" }}>
+                <Stack sx={{ padding: "10px 5%", gap: "15px" }}>
                     <Stack sx={{ flexDirection: "row", alignItems: "center" }}>
                         <img src={ProductIcon} style={{ width: "60px" }} />
                         <Typography variant='h3' sx={{ fontSize: "22px", color: "text.main" }}>Products</Typography>
                     </Stack>
                     <hr style={{ width: "100%", opacity: 0.7, borderBottom: `1px solid ${theme.palette.text.mid}` }} />
-                    <Stack sx={{ flexDirection: "row", flexWrap: "wrap",justifyContent:"space-between" }}>
+                    <Stack sx={{ flexDirection: "row",backgroundColor:"yellow" ,flexWrap: "wrap", justifyContent: "space-between", gap: "15px", paddingBottom: "20px" ,alignItems:"center"}}>
+                        <SingleProduct />
+                        <SingleProduct />
+                        <SingleProduct />
+                        <SingleProduct />
+                        <SingleProduct />
+                        <SingleProduct />
+                        <SingleProduct />
+                        <SingleProduct />
                         <SingleProduct />
                         <SingleProduct />
                         <SingleProduct />
